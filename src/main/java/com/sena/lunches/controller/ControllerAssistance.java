b2b82e50b9206ba64b5a07c906d3983cadc020b1
Kleyver11 <129457912+Kleyver11@users.noreply.github.com>
2024-03-22 10:37:09 -0500

    Assiatnce
    comtroller
    service
    impl

--- src/main/java/com/sena/lunches/controller/ControllerAssistance.java
+++ src/main/java/com/sena/lunches/controller/ControllerAssistance.java
@@ -1,13 +1,11 @@
 package com.sena.lunches.controller;
 
 import com.sena.lunches.entities.Assistance;
-import com.sena.lunches.repository.Assistance_repo;
+import com.sena.lunches.service.AssistanceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,16 +13,44 @@ import java.util.List;
 @RequestMapping("/assistance")
 public class ControllerAssistance {
     @Autowired
-    private Assistance_repo assistance_repo;
+    private AssistanceService assistanceService;
+
     @GetMapping("/listAssistance")
-    public String listAssistance(Model model) {
-        try {
-            List<Assistance> assist = assistance_repo.findAll();
-            model.addAttribute("assistance", assist);
-        }catch (Exception e) {
-            System.out.println("Error: " + e);
-        }
+    public String listUsers(Model model) {
+        List<Assistance> assistanceData = assistanceService.getAssistance();
+        model.addAttribute("assistance", assistanceData);
         return "admin/principal/list-users";
     }
 
+    @GetMapping("/newAssistance")
+    public String createNewUser(Model model){
+        model.addAttribute("assistance", new Assistance());
+        model.addAttribute("action","");
+        return "admin/principal/newAssistance";
+    }
+
+    @PostMapping("/newAssistance")
+    public String saveUserData (@ModelAttribute Assistance assistance){
+        assistanceService.saveAssistance(assistance);
+        return "redirect:/assistance/listAssistance";
+    }
+
+    @GetMapping("/editAssistance/{idAssistance}")
+    public String updateAssistance (@PathVariable Integer idAssistance, Model model){
+        model.addAttribute("assistance", assistanceService.getAssistanceById(idAssistance) );
+        model.addAttribute("action","/assistance/editAssistance/" + idAssistance);
+        return "admin/principal/newAssistance";
+    }
+
+    @PostMapping("/editAssistance/{idAssistance}")
+    public String updatingAssistance (@PathVariable Integer idAssistance,@ModelAttribute Assistance assistance){
+        assistanceService.updateAssistance(idAssistance, assistance);
+        return "redirect:/assistance/listAssistance";
+    }
+
+    @GetMapping("/delete/{idAssistance}")
+    public String deleteAssistance (@PathVariable Integer idAssistance){
+        assistanceService.deleteAssistance(idAssistance);
+        return "redirect:/assistance/listAssistance";
+    }
 }
