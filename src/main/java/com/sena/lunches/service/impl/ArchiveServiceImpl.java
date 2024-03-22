4aeb50308715a90e679d92de6508be8506428152
faiberRomero014 <faibera015@gmail.com>
2024-03-22 10:36:23 -0500

    archive
    controller
    service
    impl

---
+++ src/main/java/com/sena/lunches/service/impl/ArchiveServiceImpl.java
@@ -0,0 +1,42 @@
+package com.sena.lunches.service.impl;
+
+import com.sena.lunches.entities.Archive;
+import com.sena.lunches.repository.Archive_repo;
+import com.sena.lunches.service.ArchiveService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ArchiveServiceImpl implements ArchiveService {
+    @Autowired
+    private Archive_repo archive_repo;
+
+    @Override
+    public List<Archive> getArchive() {return archive_repo.findAll();
+    }
+
+    @Override
+    public Archive saveArchive(Archive archive) {return archive_repo.save(archive);
+    }
+
+    @Override
+    public Archive getArchiveById(Integer id) {return archive_repo.findById(id).orElse(null);
+    }
+
+    @Override
+    public Archive updateArchive(Integer id, Archive archive) {Archive oldArchive = archive_repo.findById(id).orElse(null);
+        if (oldArchive != null){
+            oldArchive.setId_excuse(archive. getId_excuse());
+            oldArchive.setId_message(archive.getId_message());
+            oldArchive.setArchive(archive.getArchive());
+            return archive_repo.save(oldArchive);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteArchive(Integer id) {archive_repo.deleteById(id);
+    }
+}
