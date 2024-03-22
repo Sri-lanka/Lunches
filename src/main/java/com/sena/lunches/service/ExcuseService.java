a6d2dd08e402441769db938ae31bf9554355e3bb
faiberRomero014 <faibera015@gmail.com>
2024-03-22 09:26:36 -0500

    excuse
    controller
    service
    impl

--- src/main/java/com/sena/lunches/service/ExcuseService.java
+++ src/main/java/com/sena/lunches/service/ExcuseService.java
@@ -1,4 +1,18 @@
 package com.sena.lunches.service;
 
+import com.sena.lunches.entities.Benefit;
+import com.sena.lunches.entities.Excuse;
+
+import java.util.List;
+
 public interface ExcuseService {
+    public List<Excuse> getExcuse();
+
+    public Excuse saveExcuse(Excuse excuse);
+
+    public Excuse getExcuseById(Integer id);
+
+    public Excuse updateExcuse(Integer id, Excuse excuse);
+
+    public void deleteExcuse(Integer id);
 }
--- src/main/java/com/sena/lunches/service/impl/ExcuseServiceImpl.java
+++ src/main/java/com/sena/lunches/service/impl/ExcuseServiceImpl.java
@@ -1,6 +1,49 @@
 package com.sena.lunches.service.impl;
 
+import com.sena.lunches.entities.Excuse;
+import com.sena.lunches.repository.Excuse_sena_repo;
 import com.sena.lunches.service.ExcuseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+
+@Service
 public class ExcuseServiceImpl implements ExcuseService {
+
+    @Autowired
+    private Excuse_sena_repo excuse_sena_repo;
+
+    @Override
+    public List<Excuse> getExcuse() {return excuse_sena_repo.findAll();
+    }
+
+    @Override
+    public Excuse saveExcuse(Excuse excuse) {
+        return excuse_sena_repo.save(excuse);
+    }
+
+    @Override
+    public Excuse getExcuseById(Integer id) {
+        return excuse_sena_repo.findById(id).orElse(null);
+    }
+
+    @Override
+    public Excuse updateExcuse(Integer id, Excuse  excuse) {
+        Excuse oldExcuse = excuse_sena_repo.findById(id).orElse(null);
+        if (oldExcuse != null){
+            oldExcuse.setId_user(excuse.getId_user());
+            oldExcuse.setId_assistance(excuse.getId_assistance());
+
+            return excuse_sena_repo.save(oldExcuse);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteExcuse(Integer id) {
+        excuse_sena_repo.deleteById(id);
+    }
+
 }
