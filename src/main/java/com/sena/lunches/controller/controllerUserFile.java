c710f338333ccab400cfd769697807f2c50e6a9c
faiberRomero014 <faibera015@gmail.com>
2024-04-02 08:52:11 -0500

    user_file
    controller
    impl

--- src/main/java/com/sena/lunches/controller/controllerUserFile.java
+++ src/main/java/com/sena/lunches/controller/controllerUserFile.java
@@ -1,15 +1,11 @@
 package com.sena.lunches.controller;
 
-
-
 import com.sena.lunches.entities.User_file;
-import com.sena.lunches.repository.User_file_sena_repo;
+import com.sena.lunches.service.UserFileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,16 +13,44 @@ import java.util.List;
 @RequestMapping("/userFile")
 public class controllerUserFile {
     @Autowired
-    private User_file_sena_repo User_file_sena_repo;
+    private UserFileService userFileService;
 
-    @GetMapping("/listUserFile")
-    public String listUserFile(Model model) {
-        try{
-        List<User_file> UserFile = User_file_sena_repo.findAll();
-        model.addAttribute("User_File", UserFile);}
-        catch (Exception e) {
-            System.out.println("Error: " + e);
-        }
+    @GetMapping("/listUser_file")
+    public String listUsers(Model model) {
+        List<User_file> user_fileData = userFileService.getUser_file();
+        model.addAttribute("user_file", user_fileData);
         return "admin/principal/list-users";
+    }
+
+    @GetMapping("/newUser_files")
+    public String createNewUser(Model model){
+        model.addAttribute("user_file", new User_file());
+        model.addAttribute("action","");
+        return "admin/principal/newUser_file";
+    }
+
+    @PostMapping("/newUser_files")
+    public String saveUserData (@ModelAttribute User_file user_file){
+        userFileService.saveUser_file(user_file);
+        return "redirect:/user_file/listUser_file";
+    }
+
+    @GetMapping("/editUser_file/{idUser_file}")
+    public String updateUser_file (@PathVariable Integer idUser_file, Model model){
+        model.addAttribute("user_file", userFileService.getUser_fileById(idUser_file) );
+        model.addAttribute("action","/user_file/editUser_file/" + idUser_file);
+        return "admin/principal/newUser_file";
+    }
+
+    @PostMapping("/editUser_file/{idUser_file}")
+    public String updatingUser_file (@PathVariable Integer idUser_file,@ModelAttribute User_file user_file){
+        userFileService.updateUser_file(idUser_file, user_file);
+        return "redirect:/user_file/listUser_file";
+    }
+
+    @GetMapping("/delete/{idUser_file}")
+    public String deleteUser_file (@PathVariable Integer idUser_file){
+        userFileService.deleteUser_file(idUser_file);
+        return "redirect:/user_file/listUser_file";
     }
 }
